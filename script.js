0b6e63e7004a54bf49e88e995262d1d51426866d
blamortar <ahmet.basaran.furkan1453@gmail.com>
2026-04-07 13:52:11 +0300

    site icin ilk deneme tasarimi

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Sistem çalışıyor, kapsül hazır!");
+
+const targetDate = new Date("Dec 31, 2026 23:59:59").getTime();
+
+const countdownTask = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+   
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    
+    document.getElementById("timer1").innerHTML = days + "g " + hours + "s " + minutes + "dk " + seconds + "sn ";
+
+    
+    if (distance < 0) {
+        clearInterval(countdownTask);
+        document.getElementById("timer1").innerHTML = "KAPSÜL AÇILDI!";
+    }
+}, 1000);
